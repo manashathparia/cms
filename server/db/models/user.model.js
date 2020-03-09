410399d68de415e95008a706fc6e1a3d7cf980fc
manash athparia <athmanash@gmail.com>
2020-03-09 22:41:04 +0530

    add time stamp to user model

--- server/db/models/user.model.js
+++ server/db/models/user.model.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const moment = require("moment");
 
 const userSchema = mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false },
-    role: {
-        type: String,
-        enum: ['subscriber', 'editor', 'author', 'administrator'],
-        default: 'subscriber',
-    },
-    specialAccess: [String],
+	username: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	dateCreated: { type: String, default: moment().format("L, h:mm:ss a") },
+	password: { type: String, required: true },
+	isAdmin: { type: Boolean, default: false },
+	role: {
+		type: String,
+		enum: ["subscriber", "editor", "author", "administrator"],
+		default: "subscriber"
+	},
+	specialAccess: [String]
 });
 
-
-
-
-mongoose.model("User", userSchema)
+mongoose.model("User", userSchema);
